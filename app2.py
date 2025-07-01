807daa91953e876fe0d8bdfbab092a91ccc96895
Deepit Garg <deepitgarg7@gmail.com>
2025-07-01 15:59:12 +0530

    Add all required packages

--- app2.py
+++ app2.py
@@ -11,7 +11,7 @@ from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain.agents.agent_types import AgentType
 
 st.set_page_config(page_title="CSV or SQL Agent", layout="centered")
-st.title("DeepQuery")
+st.title("DeepQuery Agent 🔍")
 
 # Initialize LLM once
 chat = ChatOpenAI(
